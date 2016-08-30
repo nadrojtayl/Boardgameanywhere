bcffda1d8994d5ca8e572953e09fd71072e39de4
Jordan Taylor <jordantaylor@Jordans-MacBook-Pro-2.local>
2016-08-30 13:30:48 -0700

    Let user add letters if there are any missing

--- public/client/templates.js
+++ public/client/templates.js
@@ -1,6 +1,7 @@
 var bananagramssetup = _.template("<h2 class = 'upload_form header' id='header' margin-bottom:'25px'>2. Upload your picture to start up right where you left off </h2> \
 \
 	<script src='client/cloudinarysetup.js'></script>\
+	<span> <p>3. Add letters if there are any missing </p><input type='text' id='lettertoadd'></input> <button type='button'>Add</button> </span>\
 \
 	<div class='space'  >\
 		<svg class='draggable' id = 'gameboard' width = 1000 height = 500 style = 'border:5px solid black'>\
