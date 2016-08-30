4c12d533598bbe15aaa44c9f1b015ba6b87ad213
Jordan Taylor <jordantaylor@Jordans-MacBook-Pro-2.local>
2016-08-30 14:49:34 -0700

    Build schema, with rooms, and users

--- database/database.js
+++ database/database.js
@@ -1,24 +1,53 @@
+//Set up users, games and rooms tables
+
+//a schema is the format for a table
+
+// model is an interface for interacting with an actual collection
+
 var mongoose = require('mongoose')
 var MongoClient = require('mongodb').MongoClient
 
 
-db = mongoose.connect('mongodb://localhost/test');
+db = mongoose.connect('mongodb://localhost/boardgames');
 
+var room = new mongoose.Schema({
+	gamedata:String,
+	userIds: [{type:mongoose.Schema.Types.ObjectId,ref:'User'}]
+})
 
+var Room = mongoose.model('Room',room)
+
+var user = new mongoose.Schema({
+	name: String,
+	rooms:[{type:mongoose.Schema.Types.ObjectId, ref:'Room'}]
+})
+
+var User = mongoose.model('user', user)
 
 var game = new mongoose.Schema({
-	lettersremaining: String
+	name: String,
+	rooms:[{type:mongoose.Schema.Types.ObjectId,ref:'Room'}]
 })
 
-var game = mongoose.model('Game',game);	
+var Game = mongoose.model('Game',game);	
 
-var test = new game({lettersremaining:JSON.stringify(['a','e','i','o','u'])})
+//save user brian
+var testuser = new User({name:'Brian'});
+testuser.save(function(err){
+	if(err){console.log(err)}
+})
 
-test.save(function(err){
-	if(err){
-		console.log('err');
-	}
+//save a room that brian participates in
+User.findOne({name:'Brian'},function(err,person){
+	var room = new Room({gamedata:'fakedata'})
+	room.userIds.push(person.id);
+	room.save();
 })
 
 
-//test.create();
+// //var test = new game({lettersremaining:JSON.stringify(['a','e','i','o','u'])})
+
+
+
+
+// //test.create();
