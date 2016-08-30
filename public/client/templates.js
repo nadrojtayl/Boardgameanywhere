c437adef70ea5dcad7599c7810bcfdc51213a8a5
Jordan Taylor <jordantaylor@Jordans-MacBook-Pro-2.local>
2016-08-30 11:23:40 -0700

    Save bananagrams setup into template so we can load it if user wants to play bananagrams

---
+++ public/client/templates.js
@@ -0,0 +1,11 @@
+var bananagramssetup = _.template("<h2 class = 'upload_form header' id='header' margin-bottom:'25px'>2. Upload your picture to start up right where you left off </h2> \
+\
+	<script src='client/cloudinarysetup.js'></script>\
+\
+	<div class='space'  >\
+		<svg class='draggable' id = 'gameboard' width = 1000 height = 500 style = 'border:5px solid black'>\
+			<image xlink:href='client/assets/tabletop.jpg' x= 0 y=-100 width = 1000 height=750></image>\
+		</svg>	\
+	</div>\
+	<button onclick = 'game.nextletter()''>Get next letter</button>\
+	<p> Letters remaining: <span id= 'remainingcount'>10</span> </p>")
