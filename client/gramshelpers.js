478b265456ff777229f262af4237b37927b57d04
Jordan Taylor <jordantaylor@Jordans-MacBook-Pro-2.local>
2016-08-29 18:10:49 -0700

    Line up words vertically

--- client/gramshelpers.js
+++ client/gramshelpers.js
@@ -2,8 +2,8 @@
 	var game = {};
 	function initiategame(string){
 		game.teststring = string.split("")
-		var y = 50;
-		game.teststring = game.teststring.map(function(letter,index){if(letter === " "){y=y+50};return {letter:letter,y:y,x:(32*index) + 20,offset:10}})
+		var y = 50,x=0;
+		game.teststring = game.teststring.map(function(letter,index){if(letter === " "){y=y+50;x=0;};x+=32;return {letter:letter,y:y,x:(x) + 20,offset:10}})
 		game.unusedletters = getunusedletterspile()
 		game.updateletters();
 		game.setup()
